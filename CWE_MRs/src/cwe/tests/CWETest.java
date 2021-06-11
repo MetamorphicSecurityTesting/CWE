78c1c716c9bb376874978653274104597a1b36f4
Fabrizio Pastore <fabrizio.pastore@uni.lu>
2021-06-11 18:51:06 +0200

    added

---
+++ CWE_MRs/src/cwe/tests/CWETest.java
@@ -0,0 +1,56 @@
+package cwe.tests;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import smrl.mr.crawljax.WebOperationsProvider;
+import smrl.mr.language.MRBaseTest;
+import smrl.mr.owasp.OTG_AUTHZ_002;
+
+public class CWETest extends MRBaseTest {
+	
+	private static WebOperationsProvider provider;
+	
+	private static String system = "jenkins";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		System.out.println("*** Starting time: " + getCurrentTime() +" ***");
+		
+		//by default, the SUT is the Jenkins
+		String configFile = "./testData/JenkinsICSE/jenkinsSysConfigDEMO.json";
+		
+		provider = new WebOperationsProvider(configFile);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		setProvider(provider);
+	}
+	
+	@AfterClass
+    public static void printEndingTime() {
+		System.out.println("*** Ending time: " + getCurrentTime() + " ***");
+    }   
+	
+	
+	@Test
+	public void test_() {
+		//super.test(provider,OTG_AUTHZ_002.class);
+	}
+	
+	
+	
+	public static String getCurrentTime() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		return dtf.format(now).toString();  
+	}
+
+}
