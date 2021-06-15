1cb0e084d138f0a9fbc18df22dfef95aba01f637
NazaninBayati <54472723+NazaninBayati@users.noreply.github.com>
2021-06-15 14:24:21 -0400

    update for CWE302a

---
+++ CWE_MRs/src-gen/smrl/mr/owasp/CWE_302a.java
@@ -0,0 +1,33 @@
+package smrl.mr.owasp;
+
+import java.util.List;
+import smrl.mr.language.Action;
+import smrl.mr.language.Operations;
+
+import smrl.mr.language.MR;
+
+@SuppressWarnings("all")
+public class CWE_302a extends MR{
+  public boolean mr() {
+    List<Action> _actions = Operations.Input(1).actions();
+    for (final Action action : _actions) {
+      {
+        int pos = action.getPosition();
+        {
+          ifThenBlock();
+          if (((Operations.isLogin(action) && Operations.EQUAL(Operations.Input(2), Operations.Input(1))) && Operations.Input(2).actions().get(action.getPosition()).setCookiePath(Operations.RandomCookiePath()))) {
+            ifThenBlock();
+            if ((Operations.different(Operations.Output(Operations.Input(1), pos), Operations.Output(Operations.Input(2), pos)) && Operations.isError(Operations.Output(Operations.Input(2), pos)))) {
+              expressionPass(); /* //PROPERTY HOLDS" */
+            } else {
+              return Boolean.valueOf(false);
+            }
+          } else {
+            expressionPass(); /* //PROPERTY HOLDS" */
+          }
+        }
+      }
+    }
+    return true;
+  }
+}
